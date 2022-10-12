ff1143b585dd309984c9b1c1f08573d2bc4863d7
engineerturph <engineerturph@gmail.com>
2022-10-12 13:35:19 +0300

    progress in connect x project

---
+++ kaggleenv/codelateruse.py
@@ -0,0 +1,12 @@
+# import os
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.optimizers import Adam
+
+# model = Sequential(
+#     [Dense(64, activation='relu'),
+#      Dense(32, activation='relu'),
+#      Dense(7)])
+# optimizer = Adam(learning_rate=0.001)
